cce0d6a6aafc96221d256620f3fb0ee4f892cbb3
Ali Kurucay <kurucayali@gmail.com>
2024-05-07 00:54:56 +0300

    Harmonic avarage in arrays

---
+++ src/HarmonicAvarageInArray.java
@@ -0,0 +1,12 @@
+public class HarmonicAvarageInArray {
+    public static void main(String[] args) {
+        int[] list = {1,2,3,4,5};
+        double harmonic = 0;
+        for (int i = 0; i < list.length; i++) {
+            harmonic += 1.0/ list[i];
+        }
+        double harmonicAvarage =list.length/harmonic;
+        System.out.print(harmonicAvarage);
+
+    }
+}
